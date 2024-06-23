15b660d4894d5ef5a8de57c41a07d86b4a40053a
Hiroaki Takahashi <houkoiin0202@icloud.com>
2024-06-24 05:24:06 +0900

    header_image_reload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () =>{
     
     return(
-        <img src="src/assets/headerDammy.png" style={{width: '100vw'}} alt="サンプル画像"/>
+        <img src="image/headerDammy.png" style={{width: '100vw'}} alt="サンプル画像"/>
     );
 }
 export default Header;
